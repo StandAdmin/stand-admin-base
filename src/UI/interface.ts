c0baa317633c788ab02cc1dbc3e048f5651a03f1
zhang.qiang <zhang.qiang@alibaba-inc.com>
2021-08-11 10:40:39 +0800

    FormProps

--- src/UI/interface.ts
+++ src/UI/interface.ts
@@ -1,5 +1,6 @@
 import FormItem from 'antd/es/form/FormItem';
 export { FormItem };
+export { FormProps } from 'antd/es/form';
 export { ModalProps } from 'antd/es/modal';
 export { FormInstance, FormItemProps } from 'antd/es/form';
 export { PaginationProps } from 'antd/es/pagination';
